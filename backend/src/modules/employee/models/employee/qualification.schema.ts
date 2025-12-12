4cb322a0ae301e83b261311f0d225baaec2bc3dd
User-2rxeg <eyadelmaleh07@gmail.com>
2025-12-12 13:07:33 +0200

    locking inn

--- backend/src/modules/employee/models/employee/qualification.schema.ts
+++ backend/src/modules/employee/models/employee/qualification.schema.ts
@@ -1,24 +1,25 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Types } from 'mongoose';
-import { GraduationType } from '../../enums/employee-profile.enums';
+
 import { EmployeeProfile } from './employee-profile.schema';
+import {GraduationType} from "../../enums/employee-profile.enums";
 
 @Schema({ collection: 'employee_qualifications', timestamps: true })
 export class EmployeeQualification {
-  @Prop({ type: Types.ObjectId, ref: 'EmployeeProfile', required: true })
-  employeeProfileId: Types.ObjectId;
+    @Prop({ type: Types.ObjectId, ref: 'EmployeeProfile', required: true })
+    employeeProfileId: Types.ObjectId;
 
-  @Prop({ type: String, required: true })
-  establishmentName: string;
+    @Prop({ type: String, required: true })
+    establishmentName: string;
 
-  @Prop({
-    type: String,
-    enum: Object.values(GraduationType),
-    required: true,
-  })
-  graduationType: GraduationType;
+    @Prop({
+        type: String,
+        enum: Object.values(GraduationType),
+        required: true,
+    })
+    graduationType: GraduationType;
 }
 
 export const EmployeeQualificationSchema = SchemaFactory.createForClass(
-  EmployeeQualification,
+    EmployeeQualification,
 );
