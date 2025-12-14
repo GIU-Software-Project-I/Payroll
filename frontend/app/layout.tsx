9eeb19ea5c77638e4fd3c840783dc0c52d2dab94
lusgad <lama.hassan@student.giu-uni.de>
2025-12-14 22:34:20 +0200

    running front and back

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 import { AuthProvider } from "@/app/context/AuthContext";
 
@@ -28,6 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         <AuthProvider>{children}</AuthProvider>
       </body>
